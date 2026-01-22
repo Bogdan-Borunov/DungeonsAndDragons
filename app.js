e75487f6499b5fdcfdc0cb5662343ec9b5f04965
Bogdan-Borunov <borunov.bogdan@gmail.com>
2026-01-22 16:32:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 
 tg.ready();
 tg.expand();
-
+g
 document.getElementById("btn").onclick = () => {
     tg.showAlert(`Привет, ${tg.initDataUnsafe.user.first_name}`);
 };
